3e0c4c929dd5954b0562c9e99eaca7245e8c8ccf
Tomáš  Látal <latal.tomas@gmail.com>
2023-09-17 23:21:55 +0200

    Add syntax highlighting

--- src/Layout/Markdown.elm
+++ src/Layout/Markdown.elm
@@ -1,8 +1,37 @@
 module Layout.Markdown exposing (toHtml)
 
 import Html exposing (Html)
+import Html.Attributes as Attrs
 import Markdown.Parser
-import Markdown.Renderer
+import Markdown.Renderer exposing (defaultHtmlRenderer)
+import SyntaxHighlight
+
+
+language lang =
+    case lang of
+        Just "elm" ->
+            SyntaxHighlight.elm
+
+        _ ->
+            SyntaxHighlight.noLang
+
+
+syntaxHighlight codeBlock =
+    Html.div [ Attrs.class "no-prose" ]
+        [ SyntaxHighlight.useTheme SyntaxHighlight.oneDark
+        , language codeBlock.language codeBlock.body
+            |> Result.map (SyntaxHighlight.toBlockHtml (Just 1))
+            |> Result.withDefault
+                (Html.pre [] [ Html.code [] [ Html.text codeBlock.body ] ])
+        ]
+
+
+renderer =
+    { defaultHtmlRenderer
+        | codeBlock =
+            \block ->
+                syntaxHighlight block
+    }
 
 
 toHtml :
@@ -15,7 +44,7 @@ toHtml markdownString =
         |> Result.andThen
             (\blocks ->
                 Markdown.Renderer.render
-                    Markdown.Renderer.defaultHtmlRenderer
+                    renderer
                     blocks
             )
         |> Result.withDefault [ Html.text "failed to read markdown" ]
