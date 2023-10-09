9fae173b4f2afb69fac28b6448b64bedb1d7fc21
Tomáš  Látal <latal.tomas@gmail.com>
2023-10-09 22:40:10 +0200

    add theme colours

--- elm-pages.config.mjs
+++ elm-pages.config.mjs
@@ -11,6 +11,8 @@ export default {
 <link rel="mask-icon" href="/media/logo.svg" color="#000000">
 <link rel="apple-touch-icon" href="logo-180.png">
 <meta name="generator" content="elm-pages v${context.cliVersion}" />
+<meta name="theme-color" media="(prefers-color-scheme: light)" content="#ffffff" />
+<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#0f172a" />
 `;
 //  <link rel="manifest" href="manifest.json">
 
