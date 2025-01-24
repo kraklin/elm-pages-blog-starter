991c49d511f9bd24143c3c72f2418943478e46c3
hahnah <superhahnah@gmail.com>
2025-01-24 15:55:01 +0900

    improve: Adjust tags page's structured data for SEO.

--- app/Route/Tags.elm
+++ app/Route/Tags.elm
@@ -7,11 +7,11 @@ import Content.TechBlogpost
 import FatalError exposing (FatalError)
 import Head
 import Head.Seo as Seo
-import Layout
 import Layout.Tags
 import Pages.Url
 import PagesMsg exposing (PagesMsg)
 import RouteBuilder exposing (App, StatelessRoute)
+import Settings
 import Shared
 import UrlPath
 import View exposing (View)
@@ -55,7 +55,24 @@ head :
     App Data ActionData RouteParams
     -> List Head.Tag
 head _ =
-    Layout.seoHeaders
+    let
+        imageUrl =
+            [ "media", "blog-image.png" ] |> UrlPath.join |> Pages.Url.fromPath
+    in
+    Seo.summaryLarge
+        { canonicalUrlOverride = Nothing
+        , siteName = Settings.title
+        , image =
+            { url = imageUrl
+            , alt = "logo"
+            , dimensions = Just { width = 500, height = 333 }
+            , mimeType = Nothing
+            }
+        , description = "List of Tags."
+        , locale = Settings.locale
+        , title = "Tags"
+        }
+        |> Seo.website
 
 
 view :
