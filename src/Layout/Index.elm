cfe8d2f263838f34cbda5de89a656029f5fa0179
hahnah <superhahnah@gmail.com>
2025-02-11 21:40:39 +0900

    change: Use nuetral layout for top page's blog list UI.

--- src/Layout/Index.elm
+++ src/Layout/Index.elm
@@ -3,7 +3,7 @@ module Layout.Index exposing (view)
 import Content.BlogpostCommon exposing (Metadata)
 import Html exposing (Html)
 import Html.Attributes as Attrs
-import Layout.TechBlogpost
+import Layout.Blogpost
 import Settings
 
 
@@ -13,5 +13,5 @@ view blogpostMetadata =
         [ Html.h1 [ Attrs.class "text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14" ] [ Html.text "Latest" ]
         , Html.p [ Attrs.class "text-lg leading-7 text-gray-500 dark:text-gray-400" ] [ Html.text Settings.subtitle ]
         ]
-    , Html.div [] <| List.map Layout.TechBlogpost.viewListItem blogpostMetadata
+    , Html.div [] <| List.map Layout.Blogpost.viewListItem blogpostMetadata
     ]
